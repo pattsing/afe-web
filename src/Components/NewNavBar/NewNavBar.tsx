8c4210144f79353440c54485804317500ff9a9df
Supanat Wanroj <supanat.wanroj@scbtechx.io>
2022-05-06 19:57:05 +0700

    implement responsive nav bar

--- src/Components/NewNavBar/NewNavBar.tsx
+++ src/Components/NewNavBar/NewNavBar.tsx
@@ -1,13 +1,28 @@
 import Container from "@mui/material/Container";
 import NavBarData from "../../Data/NavBarData.json";
 import "./NewNavBar.css";
-
+import Drawer from "@mui/material/Drawer";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import React, { useState } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Box from "@mui/material/Box";
+import ListItemButton from "@mui/material/ListItemButton";
 
 import nav_icon from "../../Assets/afelogo-left-white-nobg.png";
+import { useMediaQuery } from "react-responsive";
+import menuIcon from "../../Assets/menu.png";
+
+type Anchor = "top" | "left" | "bottom" | "right";
 
 const NewNavBar = () => {
+  const [isMenuClicked, setIsMenuClicked] = useState<boolean>(false);
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 768px)",
+  });
+  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
   const navigate = useNavigate();
   const location = useLocation();
   var buyNFTElement = document.getElementById("buyNFT");
@@ -40,21 +55,120 @@ const NewNavBar = () => {
     }
   };
 
+  const listTab = (tabName: String) => {
+    switch (tabName) {
+      case "FAQ":
+        return (
+          <ListItemButton onClick={() => navigate("/faq")}>
+            <ListItemText primary={tabName} />
+          </ListItemButton>
+        );
+      case "Artworks":
+        return (
+          <ListItemButton onClick={() => navigate("/artworks")}>
+            <ListItemText primary={tabName} />
+          </ListItemButton>
+        );
+      case "About Us":
+        return (
+          <ListItemButton onClick={() => navigate("/about")}>
+            <ListItemText primary={tabName} />
+          </ListItemButton>
+        );
+      case "Home":
+        return (
+          <ListItemButton onClick={() => navigate("/")}>
+            <ListItemText primary={tabName} />
+          </ListItemButton>
+        );
+      case "Environmental Themes":
+        return (
+          <ListItemButton onClick={() => navigate("/environmental")}>
+            <ListItemText primary={tabName} />
+          </ListItemButton>
+        );
+      case "How to Buy NFT":
+        return (
+          <ListItemButton onClick={() => navigate("/buy-nft")}>
+            <ListItemText primary={tabName} />
+          </ListItemButton>
+        );
+
+      default:
+        return (
+          <ListItemButton onClick={() => navigate("/")}>
+            <ListItemText primary={tabName} />
+          </ListItemButton>
+        );
+    }
+  };
+
+  const handleMenuClick = () => {
+    setIsMenuClicked(!isMenuClicked);
+  };
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setIsMenuClicked(open);
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>{NavBarData.map((text, index) => listTab(text.tabName))}</List>
+    </Box>
+  );
+
   return (
-    <div className="nav-wrapper">
-      <Container maxWidth="xl">
-        <div className="nav-wrapper">
-          <div className="nav-content-wrapper">
-            <img className="nav-icon" src={nav_icon} />
-            <div className="nav-space" />
-            <div className="nav-list">
-              {NavBarData.map((el) => {
-                return navBarTab(el.tabName);
-              })}
+    <div>
+      <div className="nav-wrapper">
+        <Container maxWidth="xl">
+          <div className="nav-wrapper">
+            <div className="nav-content-wrapper">
+              <img
+                className={isDesktopOrLaptop ? "nav-icon" : "nav-icon-mobile"}
+                src={nav_icon}
+              />
+              <div
+                className={isDesktopOrLaptop ? "nav-space" : "nav-space-mobile"}
+              />
+              {isMobile && (
+                <div>
+                  <img
+                    className="menu-icon"
+                    src={menuIcon}
+                    onClick={handleMenuClick}
+                  />
+                </div>
+              )}
+              {isDesktopOrLaptop && (
+                <div className="nav-list">
+                  {NavBarData.map((el) => {
+                    return navBarTab(el.tabName);
+                  })}
+                </div>
+              )}
             </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
+      <React.Fragment key="top">
+        <Drawer anchor="top" open={isMenuClicked} onClose={toggleDrawer(false)}>
+          {list("top")}
+        </Drawer>
+      </React.Fragment>
     </div>
   );
 };
