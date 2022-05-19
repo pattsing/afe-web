a586b4b39009e2e76ee6106eeab1eea50f21baa6
Supanat Wanroj <supanat.wanroj@scb.co.th>
2022-05-19 22:31:22 +0700

    add responsive to about

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -7,42 +7,111 @@ import derek from "../../Assets/derek.jpeg";
 import mild from "../../Assets/mild.jpeg";
 import NewNavBar from "../../Components/NewNavBar/NewNavBar";
 import Footer from "../../Components/Footer/Footer";
+import { useMediaQuery } from "react-responsive";
 
 const About = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
   const trans = translation.AboutPage;
+
+  const team = () => {
+    if (isMobile) {
+      return (
+        <div style={{ padding: "5vw" }}>
+          <div className="team-content-wrapper">
+            <div className="avatar-wrapper">
+              <img className="avatar-mobile" alt="John" src={jorn} />
+            </div>
+            <div className="morther-wrapper">
+              <div
+                className={isMobile ? "morther-title-mobile" : "morther-title"}
+              >
+                {trans.john}
+              </div>
+              <div
+                className={isMobile ? "morther-desc-mobile" : "morther-desc"}
+              >
+                {trans.johnDesc}
+              </div>
+            </div>
+          </div>
+          <div className="team-content-wrapper">
+            <div className="avatar-wrapper">
+              <img className="avatar-mobile" alt="derek" src={derek} />
+            </div>
+            <div className="morther-wrapper">
+              <div
+                className={isMobile ? "morther-title-mobile" : "morther-title"}
+              >
+                {trans.derek}
+              </div>
+              <div
+                className={isMobile ? "morther-desc-mobile" : "morther-desc"}
+              >
+                {trans.derekDesc}
+              </div>
+            </div>
+          </div>
+          <div className="team-content-wrapper">
+            <div className="avatar-wrapper">
+              <img className="avatar-mobile" alt="mild" src={mild} />
+            </div>
+            <div className="morther-wrapper">
+              <div
+                className={isMobile ? "morther-title-mobile" : "morther-title"}
+              >
+                {trans.supanat}
+              </div>
+              <div
+                className={isMobile ? "morther-desc-mobile" : "morther-desc"}
+              >
+                {trans.supanatDesc}
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="team-wrapper">
+          <div className="team-container">
+            <div className="avatar-wrapper">
+              <img className="avatar" alt="John" src={jorn} />
+            </div>
+            <div className="name">{trans.john}</div>
+            <div className="team-desc">{trans.johnDesc}&#x2122;</div>
+          </div>
+          <div className="team-container">
+            <div className="avatar-wrapper">
+              <img className="avatar" alt="derek" src={derek} />
+            </div>
+            <div className="name">{trans.derek}</div>
+            <div className="team-desc">{trans.derekDesc}</div>
+          </div>
+          <div className="team-container">
+            <div className="avatar-wrapper">
+              <img className="avatar" alt="mild" src={mild} />
+            </div>
+            <div className="name">{trans.supanat}</div>
+            <div className="team-desc">{trans.supanatDesc}</div>
+          </div>
+        </div>
+      );
+    }
+  };
 
   return (
     <div>
       <NewNavBar></NewNavBar>
-      <div className="about-wrapper" id="about">
+      <div
+        className={isMobile ? "about-wrapper-mobile" : "about-wrapper"}
+        id="about"
+      >
         <Container>
           <div className="logo-wrapper">
             <img src={whiteLogo} style={{ width: "40%" }} />
           </div>
           <div className="desc">{trans.desc}</div>
-          <div className="team-wrapper">
-            <div className="team-container">
-              <div className="avatar-wrapper">
-                <img className="avatar" alt="John" src={jorn} />
-              </div>
-              <div className="name">{trans.john}</div>
-              <div className="team-desc">{trans.johnDesc}&#x2122;</div>
-            </div>
-            <div className="team-container">
-              <div className="avatar-wrapper">
-                <img className="avatar" alt="derek" src={derek} />
-              </div>
-              <div className="name">{trans.derek}</div>
-              <div className="team-desc">{trans.derekDesc}</div>
-            </div>
-            <div className="team-container">
-              <div className="avatar-wrapper">
-                <img className="avatar" alt="John" src={mild} />
-              </div>
-              <div className="name">{trans.supanat}</div>
-              <div className="team-desc">{trans.supanatDesc}</div>
-            </div>
-          </div>
+          {team()}
         </Container>
       </div>
       <Footer></Footer>
